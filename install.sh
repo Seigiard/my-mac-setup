6a0dc2c272df123e609e68137ac87890e54a8304
Dmitry Kolosov <mitrichius@gmail.com>
2021-08-17 14:33:21 +0300

    Update configs

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 ## INSTALL OH-MY-ZSH
-brew install --cask zsh # zsh
-chsh -s /bin/zsh
+# brew install --cask zsh # zsh
+# chsh -s /bin/zsh
+
 /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
