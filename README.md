aa7ab38415900d136dc5a73ae181f4366654e015
Dmitry Kolosov <mitrichius@gmail.com>
2022-08-27 06:42:48 +0200

    Update configs

---
+++ README.md
@@ -0,0 +1,23 @@
+# Install Apps
+
+Open `install-apps.sh` and check, what do you need to be installed.
+
+If you do not need something — comment it.
+
+# Manual Settings
+
+## Fira Code
+
+You need to explicitly enable ligatures support in editors: https://github.com/tonsky/FiraCode/wiki
+
+## VS Code
+
+Install „Settings Sync“ (`Shan.code-settings-sync`) extention
+- [VS Marketplace](https://marketplace.visualstudio.com/items?itemName=Shan.code-settings-sync)
+- [Open VSX](https://open-vsx.org/vscode/item?itemName=Shan.code-settings-sync)
+
+Setup and download settings.
+
+## RayCast
+
+Open Raycast and import latest Raycast*.rayconfig
--- install-apps.sh
+++ install-apps.sh
@@ -11,14 +11,24 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+## Fonts
+brew tap homebrew/cask-fonts
+echo 'Install Fira Code with ligatures'
+brew install --cask font-fira-code
+
 brew install --cask kitty # terminal
 brew install --cask hammerspoon # window manager but not only
 brew install --cask mtmr # my touchbar my rules
 brew install --cask ticktick # to-do and task manager
-brew install --cask ilya-birman-typography-layout
 brew install --cask transmission # torrents
 brew install --cask marta # File Manager
 brew install --cask cheatsheet # Tool to list all active shortcuts of the current application
+
+brew install --cask brave-browser # Brave Browser
+brew install --cask telegram # Telegram
+
+brew install --cask ilya-birman-typography-layout
+
 
 ## Maybe
 # brew install --cask deepl # AI translator
