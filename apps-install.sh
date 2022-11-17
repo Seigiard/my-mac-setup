ce37c18854f220515c3f51333d315a50f928892f
Borisenko Andrew <seigiard@gmail.com>
2022-11-17 08:05:30 +0100

    Update apps-install.sh

--- apps-install.sh
+++ apps-install.sh
@@ -3,14 +3,13 @@
 ## INSTALL BREW
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-## INSTALL OH-MY-ZSH
-
-/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+## INSTALL ZSH
+brew install zsh
+## INSTALL ZIM
+curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 
 # Install Node using NVM.
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | zsh
 nvm install node
 
 ## Fonts
@@ -19,11 +18,14 @@ brew tap homebrew/cask-fonts
 # brew install --cask font-fira-code
 echo 'Install Input Font'
 brew install --cask font-input
+brew install --cask font-atkinson-hyperlegible
 
+brew install --cask setapp
 brew install --cask kitty # terminal
 brew install --cask marta # File Manager
-brew install --cask hammerspoon # window manager but not only
-brew install --cask raycast # raycast
+brew install --cask raycast
+# brew install --cask hammerspoon # window manager but not only
+brew install --cask dropbox
 # brew install --cask mtmr # my touchbar my rules
 brew install --cask android-file-transfer # Transfer files from and to an Android smartphone
 brew install --cask ticktick # to-do and task manager
@@ -31,11 +33,12 @@ brew install --cask transmission # torrents
 brew install --cask spotify
 brew install --cask notunes 
 
+brew install --cask webstorm
 brew install --cask vscodium
 brew install --cask obsidian
 
 # brew install --cask brave-browser # Brave Browser
-brew install --cask telegram # Telegram
+brew install --cask telegram
 
 brew install --cask vanilla # Hide icons in menu bar
 
