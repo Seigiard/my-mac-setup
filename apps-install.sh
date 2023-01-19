a79e40eb82cce71b6befc09977cf1af153525874
Andrew Borisenko <seigiard@gmail.com>
2023-01-19 03:32:27 +0100

    Migrate to hammerspoon

--- apps-install.sh
+++ apps-install.sh
@@ -24,12 +24,14 @@ brew install --cask setapp
 brew install --cask kitty # terminal
 brew install --cask marta # File Manager
 brew install --cask raycast
-brew install --cask karabiner-elements
+brew install --cask hammerspoon
+# brew install --cask karabiner-elements
 brew install yqrashawn/goku/goku
+
 brew install --cask dropbox
 # brew install --cask mtmr # my touchbar my rules
 brew install --cask android-file-transfer # Transfer files from and to an Android smartphone
-brew install --cask ticktick # to-do and task manager
+brew install --cask ticktick # to-do and task managerasdasd
 brew install --cask transmission # torrents
 brew install --cask spotify
 brew install --cask notunes
@@ -45,3 +47,9 @@ brew install --cask vanilla # Hide icons in menu bar
 
 # plugins for quick look
 brew install qlcolorcode qlstephen qlmarkdown quicklook-json betterzip qlimagesize
+sudo xattr -cr ~/Library/QuickLook/QLColorCode.qlgenerator
+sudo xattr -cr ~/Library/QuickLook/QLStephen.qlgenerator
+sudo xattr -cr ~/Library/QuickLook/QuickLookJSON.qlgenerator
+sudo xattr -cr ~/Library/QuickLook/qlImageSize.qlgenerator
+qlmanage -r
+qlmanage -r cache
